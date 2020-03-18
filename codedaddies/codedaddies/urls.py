e0d971d3bebee0b8ce0e1b59db56e9a87f9df20c
AidenTran15 <aidenkiettran@gmail.com>
2020-03-18 11:52:48 +1100

    run front-end successful

--- codedaddies/codedaddies/urls.py
+++ codedaddies/codedaddies/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from my_app import views
+
 from django.urls import path, include
 from django.contrib import admin
+from my_app import views
 
 
 
