b27848a563d9f6324caeaa3024048202415edbcc
AidenTran15 <aidenkiettran@gmail.com>
2020-03-23 17:26:01 +1100

    Test app

--- codedaddies/my_app/views.py
+++ codedaddies/my_app/views.py
@@ -24,11 +24,22 @@ def new_search(request):
     post_url = post_listings[0].find('a').get('href')
     post_price = post_listings[0].find(class_='result-price').text
 
-    print(post_title)
-    print(post_url)
-    print(post_price)
+    final_postings =[]
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        
+        if post.find(class_='result-price'):
+            post_price = post.find(class_ ='result-price').text
+        post_price = 'N/A'
+
+        final_postings.append((post_title, post_url, post_price ))
+
+    
 
     stuff_for_frontend = {
         'search': search,
+        'final_postings': final_postings 
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
